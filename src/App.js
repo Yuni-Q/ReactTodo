41761f01b3e96a235de03d5bef87c2a716be6a3e
Yuni-Q <yuni.lee@memebox.com>
2019-03-16 23:43:01 +0900

    refactor: refactor chanyeon's code review
    Fixes #2

--- src/App.js
+++ src/App.js
@@ -1,199 +1,168 @@
 import React, { Component } from 'react';
-import './App.css';
 import axios from 'axios';
-
+const URL = process.env.REACT_APP_URL;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todoList: [],
-      isAdd: false,
+      canAdd: false,
       title: '',
       content: '',
       userName: 'yuni',
-      check: false,
-      isUpdate: false,
+      isChecked: false,
+      canUpdate: false,
       id: 0,
-    };
+    }
     this.handleChange = this.handleChange.bind(this);
     this.toggleAdd = this.toggleAdd.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
-    this.preUpdateTodo = this.preUpdateTodo.bind(this);
+    this.handleTodoEdit = this.handleTodoEdit.bind(this);
     this.handleSubmitUpdate = this.handleSubmitUpdate.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  };
+  }
 
   componentDidMount() {
-    axios.get('http://13.113.246.46:8081/todos/userName/yuni')
+    axios.get(`${URL}/userName/yuni`)
       .then(result => {
         this.setState({
           todoList: result.data.result,
-        })
-      })
-      .catch(error => console.log(error));
-  };
-
-  componentDidUpdate() {
-    axios.get('http://13.113.246.46:8081/todos/userName/yuni')
-      .then(result => {
-        if(JSON.stringify(this.state.todoList) !== JSON.stringify(result.data.result)) {
-          this.setState({
-            todoList: result.data.result,
-            isAdd: false,
-            isUpdate: false,
-            title: '',
-            content: '',
-            userName: 'yuni',
-            check: 0,
-          })
-        }
-      })
-      .catch(error => console.log(error));
-  };
-
-  handleChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  toggleAdd() {
-    this.setState({
-      isAdd: true,
-      title: '',
-      content: '',
-      userName: 'yuni',
-      check: 0,
-      isUpdate: false,
-    });
-  };
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState({
-      title: this.state.title,
-      content: this.state.content,
-      check: this.state.check,
-    });
-    axios.post('http://13.113.246.46:8081/todos', {
-      title: this.state.title,
-      content: this.state.content,
-      check: this.state.check,
-      userName: this.state.userName,
-    })
-      .then(result => console.log(result))
-      .catch(error => console.error(error));
-  };
-
-  deleteTodo(id) {
-    axios.delete(`http://13.113.246.46:8081/todos/${id}}`)
-      .then(result => {
-        this.forceUpdate();
-        console.log(result)
-      })
-      .catch(error => console.error(error));
-  };
-
-  preUpdateTodo(id) {
-    axios.get(`http://13.113.246.46:8081/todos/${id}}`)
-      .then(result => {
-        this.setState({
-          isAdd: false,
-          title: result.data.result.title,
-          content: result.data.result.content,
-          check: result.data.result.check,
-          isUpdate: true,
-          id,
         });
       })
       .catch(error => console.error(error));
-  };
+  }
 
-  handleSubmitUpdate(event) {
-    console.log(this.state.check);
-    event.preventDefault();
+  handleChange(event) {
+    const { type, checked, value, name } = event.target;
     this.setState({
-      title: this.state.title,
-      content: this.state.content,
-      check: this.state.check,
+      [name]: type === 'checkbox' ? checked : value,
     });
-    axios.put(`http://13.113.246.46:8081/todos/${this.state.id}`, {
-      title: this.state.title,
-      content: this.state.content,
-      check: this.state.check,
-      userName: this.state.userName,
+  }
+
+  toggleAdd() {
+    this.setState(state => ({
+      canAdd: !state.canAdd,
+      title: '',
+      content: '',
+      userName: 'yuni',
+      isChecked: 0,
+      canUpdate: false,
+    }));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { title, content, isChecked, userName } = this.state;
+    axios.post(URL, {
+      title,
+      content,
+      check: isChecked,
+      userName,
     })
+      .then(() => {
+        return axios.get(`${URL}/userName/yuni`)
+      })
       .then(result => {
-        this.forceUpdate();
-        console.log(result);
+        this.setState({
+          canAdd: false,
+          todoList: result.data.result,
+        });
       })
       .catch(error => console.error(error));
-  };
+  }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-    console.log(name,value,this.state.check);
-  };
+  deleteTodo(id) {
+    axios.delete(`${URL}/${id}}`)
+      .then(() => {
+        return axios.get(`${URL}/userName/yuni`)
+      })
+      .then(result => {
+        this.setState({
+          todoList: result.data.result,
+        });
+      })
+      .catch(error => console.error(error));
+  }
+
+  handleTodoEdit(id) {
+    axios.get(`${URL}/${id}}`)
+      .then(result => {
+        const { title, content, check } = result.data.result;
+        this.setState({
+          canAdd: false,
+          canUpdate: true,
+          content,
+          id,
+          isChecked: check,
+          title,
+        });
+      })
+      .catch(error => console.error(error));
+  }
+
+  handleSubmitUpdate(event) {
+    event.preventDefault();
+    const { title, content, isChecked, userName } = this.state;
+    axios.put(`${URL}/${this.state.id}`, {
+      title,
+      content,
+      check: isChecked,
+      userName,
+    })
+      .then(() => {
+        return axios.get(`${URL}/userName/yuni`)
+      })
+      .then(result => {
+        this.setState({
+          canUpdate: false,
+          todoList: result.data.result,
+        });
+      })
+      .catch(error => console.error(error));
+  }
 
   render() {
-    const { todoList } = this.state;
-    console.log(todoList);
+    const { todoList, canAdd, canUpdate } = this.state;
     return (
       <>
         <h1>지금부터 웹을 만들어 보겠습니다.</h1>
         <div>할일이 {todoList.length}개 있습니다.</div>
         <button onClick={this.toggleAdd}>추가</button>
         <table border={1}>
-        <thead>
-          <tr>
-            <th>title</th>
-            <th>content</th>
-            <th>check</th>
-            <th>수정</th>
-            <th>삭제</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>title</th>
+              <th>content</th>
+              <th>check</th>
+              <th>수정</th>
+              <th>삭제</th>
+            </tr>
           </thead>
           <tbody>
-          {todoList.map((todo) => {
-            return (
-              <tr key={todo.id}>
-                <td>{todo.title}</td>
-                <td>{todo.content}</td>
-                <td><input type="checkbox" name="chexck" checked={todo.check===1} readOnly/></td>
-                <td onClick={() => this.preUpdateTodo(todo.id)}><button>수정</button></td>
-                <td onClick={() => this.deleteTodo(todo.id)}><button>삭제</button></td>
-              </tr>
-            )
-          })}
+            {todoList.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td>{todo.title}</td>
+                  <td>{todo.content}</td>
+                  <td><input type="checkbox" name="isChecked" checked={todo.check} readOnly /></td>
+                  <td><button onClick={() => this.handleTodoEdit(todo.id)}>수정</button></td>
+                  <td onClick={() => this.deleteTodo(todo.id)}><button>삭제</button></td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
-        {this.state.isAdd && (
-          <form onSubmit={this.handleSubmit}>
-          <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/><br/>
-          <input type="text" name="content" value={this.state.content} onChange={this.handleChange}/><br/>
-          <input type="submit" value="Submit"/>
-        </form>
-        )}
-        {this.state.isUpdate && (
-          <form onSubmit={this.handleSubmitUpdate}>
-          <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/><br/>
-          <input type="text" name="content" value={this.state.content} onChange={this.handleChange}/><br/>
-          <input type="checkbox" name="check" value={this.state.check===1 || this.state.check} checked={this.state.check===1 || this.state.check} onChange={this.handleChange}/><br/>
-          <input type="submit" value="Submit"/>
-        </form>
+        {(!!canAdd || !!canUpdate) && (
+          <form onSubmit={canAdd ? this.handleSubmit : this.handleSubmitUpdate}>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} /><br />
+            <input type="text" name="content" value={this.state.content} onChange={this.handleChange} /><br />
+            {canUpdate && (<><input type="checkbox" name="isChecked" value={this.state.isChecked} checked={this.state.isChecked} onChange={this.handleChange} /><br /></>)}
+            <input type="submit" value="submit" />
+          </form>
         )}
       </>
-    );
+    )
   }
 }
 
